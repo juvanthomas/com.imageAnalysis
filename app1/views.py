6a57bb37f8027c69416f78ae683a02cff22dc605
testuser <juvantk@gmail.com>
2019-07-20 12:58:08 +0530

    logic change in app1 views

--- app1/views.py
+++ app1/views.py
@@ -8,20 +8,29 @@ def ast1(request):
     context = {}
     if request.method == 'POST':
         uploaded_file = request.FILES['document']
-        print(uploaded_file)
         fs = FileSystemStorage()
         name = fs.save(uploaded_file.name, uploaded_file)
         context['url'] = fs.url(name)
-        img=cv2.imread(fs.path(name),0)
+        img=cv2.imread(fs.path(name))
         image = cv2.resize(img, (600, 600))
-        value = (np.sum(image))
-        if value < 10000000:
-            text="Camera is covered"
-        elif value > 82800000:
-            text="Camera is over lighted"
+        imagegray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        value = (np.sum(imagegray))
+        r = image[:, :, 0]
+        g = image[:, :, 1]
+        b = image[:, :, 2]
+        std = (imagegray.std())
+        stdr = (r.std())
+        stdg = (g.std())
+        stdb = (b.std())
+        if (stdr < 15) or (stdg < 15) or (stdb < 15) or (std<10):
+            if value < 10000000:
+                text = "Camera  is covered"
+            elif value > 82000000:
+                text = "Camera is over lighted"
+            else:
+                text = "Normal Picture"
         else:
-            text="Normal picture"
-        print(text,value)
+            text = "Normal Picture"
         context['result']=text
         context['path']=fs.path(name)
     return render(request, 'assignment1.html', context)
